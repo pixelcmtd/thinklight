54c863fe275331954b216c27c1f2c5f6c52f46b4
chrissx <chrissx@chrissx.ga>
2019-06-09 14:09:46 +0200

    added power & wifi leds

--- config.h
+++ config.h
@@ -7,4 +7,10 @@
 #elif defined(TL_SB)
 #define PATH \
         "/sys/class/leds/tpacpi::standby/brightness"
+#elif defined(TL_PW)
+#define PATH \
+	"/sys/devices/platform/thinkpad_acpi/leds/tpacpi::power/brightness"
+#elif defined(TL_WL)
+#define PATH \
+	"/sys/class/leds/phy0-led/brightness"
 #endif
